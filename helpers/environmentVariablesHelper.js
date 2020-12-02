da340681e8a4be3824a5a60521bbf4f80e15c8dc
nani3592 <venkatkadiveti248@gmail.com>
2020-12-02 15:51:31 +0530

    Issue #SB-21326 fix: adding new apis for getting forum id

--- helpers/environmentVariablesHelper.js
+++ helpers/environmentVariablesHelper.js
@@ -5,7 +5,7 @@ const __envIn = 'dev';
 let devEnvVariables = {
     DISCUSSION_FORUM: env.sunbird_discussion_forum || 'http://nodebb-service:4567/api',
     Authorization: 'a4838b88-6a04-4293-a504-245862cad404',
-    MONGODB_CONNECTION_URL: 'mongodb://localhost:27017/'
+    MONGODB_CONNECTION_URL: env.mongodb_connection_url || 'mongodb://localhost:27017/'
 }
 
 module.exports = devEnvVariables;
