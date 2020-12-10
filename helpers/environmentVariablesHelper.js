81c9cd808bcef78febaa4dd49f361138f800a7ec
nani3592 <venkatkadiveti248@gmail.com>
2020-12-10 13:48:43 +0530

    Issue SH-21326 fix: Discussion forum middleware api envs

--- helpers/environmentVariablesHelper.js
+++ helpers/environmentVariablesHelper.js
@@ -3,9 +3,9 @@ const env = process.env
 const __envIn = 'dev';
 
 let devEnvVariables = {
-    DISCUSSION_FORUM: env.sunbird_discussion_forum || 'https://dev.sunbirded.org/discussions/api', // 'http://nodebb-service:4567/api',
-    Authorization:  '9c1adb65-14a9-421d-be75-6006f49c85b6',
-    MONGODB_CONNECTION_URL: env.mongodb_connection_url || 'mongodb://11.2.3.75:27017/'
+    DISCUSSION_FORUM: env.sunbird_discussion_forum || 'https://dev.sunbirded.org/discussions/api',
+    Authorization:  env.authorization_token,
+    MONGODB_CONNECTION_URL: env.mongodb_connection_url
 }
 
 module.exports = devEnvVariables;
