ee48d3b1d77744ea4d9cb7ecd13d82a23d993899
nani3592 <venkatkadiveti248@gmail.com>
2020-12-18 17:16:46 +0530

    issue sh-21326 fix: discussion envs

--- helpers/environmentVariablesHelper.js
+++ helpers/environmentVariablesHelper.js
@@ -3,7 +3,7 @@ const env = process.env
 const __envIn = 'dev';
 
 let devEnvVariables = {
-    NODEBB_SERVICE_URL: env.nodebb_service_url || 'https://dev.sunbirded.org/discussions/api',
+    NODEBB_SERVICE_URL: env.nodebb_service_url || 'http://nodebb-service:4567/api',
     Authorization:  env.authorization_token,
     MONGODB_CONNECTION_URL: env.mongodb_connection_url
 }
