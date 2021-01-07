2e8f4467864dd2450da65b80d148cdba93ac5019
nani3592 <venkatkadiveti248@gmail.com>
2021-01-07 16:57:22 +0530

    Issue #SB-22183 fix: adding envs

--- helpers/environmentVariablesHelper.js
+++ helpers/environmentVariablesHelper.js
@@ -5,7 +5,7 @@ const __envIn = 'dev';
 let devEnvVariables = {
     NODEBB_SERVICE_URL: env.nodebb_service_url || 'https://dev.sunbirded.org', //'http://nodebb-service:4567/api',
     Authorization:  env.authorization_token || '9c1adb65-14a9-421d-be75-6006f49c85b6',
-    Nodebb_Api_Slug: env.Nodebb_Api_Slug || '/discussions/api'
+    Nodebb_Api_Slug: env.nodebb_api_slug || '/discussions/api'
 }
 
 module.exports = devEnvVariables;
