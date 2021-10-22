80470ed4a9d15ef06b87b77c155a3168b6ee28ad
Kamaleeswary <kamaleeanbu@gmail.com>
2021-10-22 18:01:42 +0530

    Issue #SB-26780 feat: Secure API - User profile update as private end point

--- routes/nodebb.js
+++ routes/nodebb.js
@@ -34,8 +34,9 @@ app.post(`${BASE_REPORT_URL}/forum/v2/read`, proxyObject());
 app.post(`${BASE_REPORT_URL}/forum/v2/create`, proxyObject());
 app.post(`${BASE_REPORT_URL}/forum/v2/remove`, proxyObject());
 app.post(`${BASE_REPORT_URL}/forum/v3/create`, proxyObject());
-app.post(`${BASE_REPORT_URL}/forum/tags`, proxyObject())
+app.post(`${BASE_REPORT_URL}/forum/tags`, proxyObject());
 app.post(`${BASE_REPORT_URL}/privileges/v2/copy`, proxyObject());
+app.post(`${BASE_REPORT_URL}/forum/v3/user/profile`, proxyObject());
 
 app.post(`${BASE_REPORT_URL}/forum/v3/group/membership`, proxyObject());
 app.post(`${BASE_REPORT_URL}/forum/v3/groups/users`, proxyObject());
