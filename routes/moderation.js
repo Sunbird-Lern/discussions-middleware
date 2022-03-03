d4c3bea5c554d5ef694db0c7c467c270f7168bf4
arunpilli21 <arunkumar.pilli@tarento.com>
2022-03-03 13:43:46 +0530

    added uid

--- routes/moderation.js
+++ routes/moderation.js
@@ -5,7 +5,7 @@ const nodebbServiceUrl = NODEBB_SERVICE_URL + nodebb_api_slug;
 exports.deleteTopic = async (body) => {
     try {
         console.log(body, body.response)
-        const response = await axios.delete(`${nodebbServiceUrl}/v2/topics/${body.response}`, {
+        const response = await axios.delete(`${nodebbServiceUrl}/v2/topics/${body.response}?_uid=1`, {
             headers: { 'Authorization': 'Bearer ' + Authorization },
         })
         console.log(response)
